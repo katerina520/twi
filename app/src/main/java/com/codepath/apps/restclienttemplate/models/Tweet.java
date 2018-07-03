ea512c4fd278552766871ee0f62b5d947b4aad2e
Ekaterina Korovina <splendidavocado@fb.com>
2018-07-03 10:16:37 -0700

    shows tweets

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -0,0 +1,28 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Tweet {
+    // list out the attributes
+    public String body;
+    public long uid; // db id for the tweet
+    public User user;
+    public String createdAt;
+    // desereilize the json
+    public static Tweet fromJSON(JSONObject jsonObject) throws JSONException{
+        Tweet tweet = new Tweet();
+        // extract the values from json
+        tweet.body = jsonObject.getString("text");
+        tweet.uid = jsonObject.getLong("id");
+        tweet.createdAt = jsonObject.getString("created_at");
+        tweet.user = User.fromJSON(jsonObject.getJSONObject("user"));
+
+        return tweet;
+
+
+
+    }
+
+
+}
